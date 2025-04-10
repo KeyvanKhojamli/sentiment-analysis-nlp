4c18fd97aaeb65991de785bfa752b0bca496eb81
keyvan kh <k1.khojamli@gmail.com>
2025-04-11 02:36:28 +0330

    update main and save the model

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import joblib
 
 
 #load data
@@ -12,7 +13,7 @@ df = pd.read_csv("data/imdb.csv")
 
 
 #split data
-X_train , X_test , Y_train , Y_test = train_test_split(df["text"], df["label"] ,test_size = 0.9, random_state = 42)
+X_train , X_test , Y_train , Y_test = train_test_split(df["text"], df["label"] ,test_size = 0.2, random_state = 142)
 
 #build pipeline
 pipeline =Pipeline([
@@ -26,3 +27,5 @@ pipeline.fit(X_train,Y_train)
 #evaluate 
 y_pred = pipeline.predict(X_test)
 print("Acuracy : " , accuracy_score(Y_test ,y_pred))
+
+joblib.dump(pipeline , "model/sentiment_model.pkl")
