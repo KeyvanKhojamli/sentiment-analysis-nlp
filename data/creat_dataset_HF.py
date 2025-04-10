776e82a8bee227652bd76efce4d7fa1150f4dc16
keyvan kh <k1.khojamli@gmail.com>
2025-04-11 00:36:20 +0330

    add dataset from huggingface that contain 10000 train example

---
+++ data/creat_dataset_HF.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset
+import pandas as pd
+
+dataset = load_dataset("imdb" ,split="train[:10000]")
+
+df = pd.DataFrame({
+    "text" : dataset["text"],
+    "label" : ["positive" if l ==1 else "negetive" for l in dataset["label"]]
+})
+
+df.to_csv("data/imdb.csv" , index = False)
+print(df.head(10))
+print(df.__len__())
+print("DONE!!")
