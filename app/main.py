d77bc16e66773e5f25db4b0fb2be1500da161811
keyvan kh <k1.khojamli@gmail.com>
2025-04-11 02:22:14 +0330

    create a model

---
+++ app/main.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+#load data
+df = pd.read_csv("data/imdb.csv")
+
+
+
+#split data
+X_train , X_test , Y_train , Y_test = train_test_split(df["text"], df["label"] ,test_size = 0.9, random_state = 42)
+
+#build pipeline
+pipeline =Pipeline([
+    ("tfidf", TfidfVectorizer()),
+    ("model",MultinomialNB())
+])
+
+#train
+pipeline.fit(X_train,Y_train)
+
+#evaluate 
+y_pred = pipeline.predict(X_test)
+print("Acuracy : " , accuracy_score(Y_test ,y_pred))
